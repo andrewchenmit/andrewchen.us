62d033b6db610a4c44a6b43c5486459d1e201acd
Andrew Chen <moorheadandrew@gmail.com>
2014-01-26 22:18:38 -0800

    Under construction version.

--- andrewchenproxy/www/js/app.js
+++ andrewchenproxy/www/js/app.js
@@ -2,6 +2,6 @@ angular.module('example', []).
   config(['$routeProvider', function($routeProvider) {
   $routeProvider.
       when('/', {templateUrl: '/www/partials/home.html'}).
-      when('/example', {templateUrl: '/www/partials/example.html', controller: ExamplesCtrl}).
+      when('/bio', {templateUrl: '/www/partials/bio.html', controller: ExamplesCtrl}).
       otherwise({redirectTo: '/'});
 }]);
