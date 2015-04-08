3f09fea0778e8ed3b018df788a6d22f36394eb50
Andrew Chen <moorheadandrew@gmail.com>
2015-04-07 23:12:56 -0700

    crappy display of data

--- andrewchenproxy/www/travelroos/js/controllers.js
+++ andrewchenproxy/www/travelroos/js/controllers.js
@@ -16,12 +16,28 @@ function ExamplesCtrl($scope, $http) {
 var phonecatApp = angular.module('phonecatApp', []);
 
 phonecatApp.controller('PhoneListCtrl', function ($scope, $http) {
+  function format_data(d) {
+    result = {};
+    for (var i=0;i<d.length;i++) {
+      if (!(d[i].destination_airport in result)) {
+        result[d[i].destination_airport] = {};
+      }
+      if (!(d[i].there_date in result[d[i].destination_airport])) {
+        result[d[i].destination_airport][d[i].there_date] = {};
+      }
+      if (!(d[i].check_date in result[d[i].destination_airport][d[i].there_date])) {
+        result[d[i].destination_airport][d[i].there_date][d[i].check_date] = d[i];
+      }
+    }
+    return result;
+  }
   console.log("SD");
   $http.get("weekendfaresdb")
     .success(function(data){
+      //$scope.phones = format_data(data);
       $scope.phones = data;
       console.log("SUCCESS");
-      console.log(data);
+      console.log(format_data(data));
     })
     .error(function() {
       $scope.phones = "error in fetching data";
